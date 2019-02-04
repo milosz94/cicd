af27a2d804990296511b88573bbb662944775260
Milos Zivanovic <tihiway@hotmail.com>
2019-02-04 19:12:34 +0100

    init

---
+++ src/main/java/com/milosz94/cicd/CicdApplication.java
@@ -0,0 +1,14 @@
+package com.milosz94.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/milosz94/cicd/CicdApplicationTests.java
@@ -0,0 +1,17 @@
+package com.milosz94.cicd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
